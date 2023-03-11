82a40f94e84f4a0f01e8e2581ff3ba43e5b432a8
Gillesedgy <edgygilles@pursuit.org>
2023-03-11 10:01:19 -0500

    added soutions to multiply and Variable assignments

---
+++ 8kyu/multiply.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+//* CODE
+// function multiply(a, b){
+//   a * b
+// }
+
+//! Solution(s)
+function multiply(a, b){
+    return a*b;
+  }
+   
+  
+  
---
+++ 8kyu/variableAssignment.js
@@ -0,0 +1,13 @@
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+//* Code
+// var a = "dev"
+// var b = "Lab"
+
+// var name = a + b
+
+//! Solution
+const a = `dev`;
+const b = `Lab`;
+const name = `${a}${b}`;
